71862227a2a443bbf427b8d6f6c236931a3c5916
lrandres <156129115+lrandres@users.noreply.github.com>
2024-10-13 22:03:14 -0400

    Update action-slugs.sh

--- archive/backfill/action-slugs.sh
+++ archive/backfill/action-slugs.sh
@@ -1,6 +1,10 @@
 # gh run list --limit 1000 > action-runs.txt
 
 filename="action-runs.txt"
+
+echo "Processing file: $filename"
+
 while read -r line; do
-  gh run view $line --log | grep "Z data/"
+    echo "Processing run ID: $line"
+    gh run view "$line" --log | grep "Z data/"
 done < "$filename"
