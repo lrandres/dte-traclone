11b62871a0e59debc457759131392bd0b2671608
lrandres <156129115+lrandres@users.noreply.github.com>
2024-10-13 22:52:17 -0400

    Update action-slugs.sh

--- archive/backfill/action-slugs.sh
+++ archive/backfill/action-slugs.sh
@@ -4,9 +4,10 @@ filename="action-runs.txt"
 
 echo "Processing file: $filename"
 
+# Loop through each line in the file
 while read -r line; do
-    # Extract the run ID (7th column)
-    run_id=$(echo "$line" | awk '{print $7}')
+    # Extract the run ID (11th column)
+    run_id=$(echo "$line" | awk '{print $11}')
     
     # Debugging: Print the run ID being processed
     echo "Processing run ID: $run_id"
